14c2071bdb3fe50446bcb099dbf769158c4fbe31
Samihan <samihannandedkar15@gmail.com>
2026-01-18 18:53:23 +0530

    modifications in index.js

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 Production CI/CD Node App Running");
+  res.send("Production CI/CD Node App Running");
 });
 
 app.listen(3000, "0.0.0.0", () => {
